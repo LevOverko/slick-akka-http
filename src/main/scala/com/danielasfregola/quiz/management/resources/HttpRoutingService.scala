6888ecf08b77dc7daef86030bfd121474687f77d
Lev <leva.work.90@gmail.com>
2022-02-14 17:24:50 +0300

    compose db

--- src/main/scala/com/danielasfregola/quiz/management/resources/HttpRoutingService.scala
+++ src/main/scala/com/danielasfregola/quiz/management/resources/HttpRoutingService.scala
@@ -1,7 +1,6 @@
 package com.danielasfregola.quiz.management.resources
 
 import com.danielasfregola.quiz.management.component.{ActorSystemComponent, Repositories}
-import com.danielasfregola.quiz.management.resources.UserResource
 import com.danielasfregola.quiz.management.serializers.Serializers
 
 trait HttpRoutingService extends HttpBaseService with UserResource {
